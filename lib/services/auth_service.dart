0b881b7b93ecaed9133022d94397cdeaf5eba1fe
binshadI <binshadbinshad729@gmail.com>
2026-02-16 00:58:23 +0530

    register req,res creatd & add auth servicess for reg

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,6 +5,9 @@ import '../config/config.dart';
 import '../models/login_request_model.dart';
 import '../models/login_response_model.dart';
 import '../services/shared_service.dart';
+import '../models/register_request_model.dart';
+import '../models/register_response_model.dart';
+
 
 class AuthService {
   static var client = http.Client();
@@ -38,6 +41,36 @@ class AuthService {
 
       await SharedService.setLoginDetails(responseModel);
 
+      return null;
+    } else {
+      return jsonData["message"];
+    }
+  }
+
+
+  //register section ..._________________
+
+
+  static Future<String?>register(RegisterRequestModel model) async {
+    print("register page is working..");
+
+    Map<String, String> requestHeader = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.parse("${Config.apiURL}${Config.registerApi}");
+
+    var response = await client.post(
+      url,
+      headers: requestHeader,
+      body: jsonEncode(model.toJson()),
+    );
+
+    var jsonData = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      RegisterResponseModel responseModel =
+        RegisterResponseModel.fromJson(jsonData);
       return null;
     } else {
       return jsonData["message"];
